e447816758ff18b42565800f84d0563974452226
Sakimori <sakimorihalley@gmail.com>
2020-08-03 14:17:05 -0400

    Selectable list populated by team names from live server

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,21 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.debugButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 65);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(423, 303);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // debugButton
+            // 
+            this.debugButton.Location = new System.Drawing.Point(441, 81);
+            this.debugButton.Name = "debugButton";
+            this.debugButton.Size = new System.Drawing.Size(347, 23);
+            this.debugButton.TabIndex = 1;
+            this.debugButton.Text = "button1";
+            this.debugButton.UseVisualStyleBackColor = true;
+            this.debugButton.Click += new System.EventHandler(this.debugButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(441, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Team ID";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.debugButton);
+            this.Controls.Add(this.listBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button debugButton;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,19 +10,122 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.Security.Permissions;
+using System.Diagnostics;
+using SocketIOClient;
+using Quobject.EngineIoClientDotNet.Client;
+using System.Drawing.Text;
 
 namespace Blaseball_Livestream
 {
     public partial class Form1 : Form
     {
+        Client formClient;
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            formClient = new Client();
+            System.Threading.Thread.Sleep(1000);
+            List<Team> allTeams = await formClient.GetAllTeams();
+            while (allTeams == null) { System.Threading.Thread.Sleep(3000); }
+            allTeams.Sort();
+            foreach(Team t in allTeams)
+            {
+                listBox1.Items.Add(t);
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
+
+        private void debugButton_Click(object sender, EventArgs e)
+        {
+            Team selected = listBox1.SelectedItem as Team;
+        }
     }
+    class Client
+    {
+        static HttpClient webClient = new HttpClient();
+
+        public async void OpenSocket()
+        {
+
+        }
+
+        public async Task<Team> GetTeamFromName(string searchName)
+        {
+            HttpResponseMessage response = await webClient.GetAsync("https://blaseball.com/database/allTeams");
+            //Debug.WriteLine(await response.Content.ReadAsStringAsync());
+            if (response.IsSuccessStatusCode)
+            {
+                string responseString = await response.Content.ReadAsStringAsync();
+                List<Team> allTeams = JsonConvert.DeserializeObject<List<Team>>(responseString);
+                //Debug.WriteLine(allTeams);
+                foreach(Team t in allTeams)
+                {
+                    //Debug.WriteLine(t.fullName);
+                    //Debug.WriteLine(t.nickname);
+                    if(t.nickname == searchName || t.fullName == searchName)
+                    {
+                        return t;
+                    }
+                }
+            }
+            return null;
+        }
+
+        public async Task<List<Team>> GetAllTeams()
+        {
+            HttpResponseMessage response = await webClient.GetAsync("https://blaseball.com/database/allTeams");
+            if (response.IsSuccessStatusCode)
+            {
+                return JsonConvert.DeserializeObject<List<Team>>(await response.Content.ReadAsStringAsync());
+            }
+            return null;
+        }
+    }
+
+    public class Team:IComparable<Team>
+    {
+        public string _id { get; set; }
+        public string fullName { get; set; }
+        public string nickname { get; set; }
+        public string mainColor { get; set; }
+        public int CompareTo(Team other)
+        {
+            return nickname.CompareTo(other.nickname);
+        }
+        public override string ToString()
+        {
+            return nickname;
+        }
+    }
+
+    public class Game
+    {
+        public string id { get; set; }
+        public Team awayTeam { get; set; }
+        public Team homeTeam { get; set; }
+        public List<Inning> innings { get; set; }
+    }
+
+    public class Inning:IComparable<Inning>
+    {
+        public int number { get; set; }
+        public int awayScore { get; set; }
+        public int homeScore { get; set; }
+        public int CompareTo(Inning other)
+        {
+            return number.CompareTo(other.number);
+        }
+    }
+
 }
